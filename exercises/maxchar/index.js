4ea656714a0db95706f6130e7cc275e8a0f16829
chris-yi <kri5to.yi@gmail.com>
2017-12-27 17:15:35 -0800

    completed maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const mapChar = {};
+    let maxNum = 0
+    let maxChar = ""
+    
+    for(let i = 0; i < str.length; i++) {
+      if(mapChar[str[i]]) {
+        mapChar[str[i]]++
+      } else {
+        mapChar[str[i]] = 1
+      }
+    }
+    
+    for(let i in mapChar) {
+      if(mapChar[i] > maxNum) {
+        maxNum = mapChar[i]
+        maxChar = i
+      }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
