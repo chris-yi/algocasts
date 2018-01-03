9d97b4bd74108e4ff30fcc0d48e52809d3c49d37
chris-yi <kri5to.yi@gmail.com>
2018-01-02 17:02:04 -0800

    solved Anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,11 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const newA = stringA.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
+    const newB = stringB.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
+    
+    return newA === newB
+  }
 
 module.exports = anagrams;
