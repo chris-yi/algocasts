9956f27379e8942f08e3874ee906c8c6cfcaab60
chris-yi <kri5to.yi@gmail.com>
2018-01-03 17:26:31 -0800

    solved using character maps (reference this for character map)

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,11 +8,42 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
+// function anagrams(stringA, stringB) {
+//     const newA = stringA.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
+//     const newB = stringB.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
+
+//     return newA === newB
+//   }
+
+
+
+
+// Solving using character maps
 function anagrams(stringA, stringB) {
-    const newA = stringA.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
-    const newB = stringB.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("")
-    
-    return newA === newB
-  }
+    const aCharMap = buildCharMap(stringA);
+    const bCharMap = buildCharMap(stringB);
+
+    if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+        return false;
+    }
+
+    for (let char in aCharMap) {
+        if(aCharMap[char] !== bCharMap[char]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Helper Function
+function buildCharMap(str) {
+    const charMap = {};
+
+    for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+}
 
 module.exports = anagrams;
